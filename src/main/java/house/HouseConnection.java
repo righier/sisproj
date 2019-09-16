7c9a0c2486cc7e895285a0081d1db1806d85a7d6
Ermanno <ermannorighini@gmail.com>
2019-09-16 16:09:20 +0200

    dio cane

---
+++ src/main/java/house/HouseConnection.java
@@ -0,0 +1,203 @@
+package house;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.protobuf.Any;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
+import com.google.protobuf.MessageLite;
+
+import beans.Measurement;
+import proto.HouseProto.*;
+import utils.MessageQueue;
+import utils.MessageWait;
+
+public class HouseConnection implements Runnable {
+
+	private HouseManager manager;
+
+	private Socket socket;
+	private InputStream in;
+	private OutputStream out;
+
+	private MessageQueue queue;
+
+	private volatile boolean alive = true;
+
+	private String id = null;
+	
+	private List<MessageWait> waitLocks = new ArrayList<>();
+
+	public HouseConnection(HouseManager manager, String remoteId, String host, int port)
+			throws IOException {
+		this.id = remoteId;
+
+		Socket socket = new Socket(host, port);
+		init(manager, socket);
+
+		manager.add(this);
+
+		Any msg = Any.pack(Hello.newBuilder().setId(manager.getLocalId()).build());
+		write(msg);
+		
+		System.out.println("hello sent to: "+remoteId+", "+host+":"+port);
+		
+		waitMessage(Welcome.class);
+	}
+
+	public HouseConnection(HouseManager manager, Socket socket) throws IOException {
+		System.out.println("connection received, port: "+socket.getPort());
+		init(manager, socket);
+	}
+	
+	public void waitMessage(Class<? extends Message> c) {
+		new MessageWait(this).wait(c);
+	}
+	
+	public void addWaitLock(MessageWait w) {
+		synchronized(waitLocks) {
+			waitLocks.add(w);
+		}
+	}
+	
+	public void removeWaitLock(MessageWait w) {
+		synchronized(waitLocks) {
+			waitLocks.remove(w);
+		}
+	}
+
+	private void init(HouseManager manager, Socket socket) throws IOException {
+		this.manager = manager;
+		this.socket = socket;
+		this.in = socket.getInputStream();
+		this.out = socket.getOutputStream();
+
+		this.queue = new MessageQueue(this.out);
+
+		new Thread(this);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void write(MessageLite msg) {
+		queue.add(msg);
+	}
+
+	public void kill() {
+		alive = false;
+		manager.remove(this.id);
+		
+		queue.stop();
+		queue.waitEmpty();
+
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+
+		while (alive) {
+			Any msg;
+			try {
+				msg = Any.parseDelimitedFrom(in);
+			} catch (IOException e) {
+				e.printStackTrace();
+				continue;
+			}
+			
+			System.out.println(msg);
+
+			try {
+				if (msg.is(Hello.class)) {
+					Hello hello = msg.unpack(Hello.class);
+					this.id = hello.getId();
+					manager.add(this);
+
+					MeasureList.Builder list = MeasureList.newBuilder();
+					for (Measurement m: manager.getMeasurements()) {
+						list.addMeasures(m.toProtobuf());
+					}
+
+					write(Any.pack(list.build()));
+					write(Any.pack(Welcome.newBuilder().build()));
+
+				} else if (msg.is(Goodbye.class)) {
+					manager.remove(id);
+
+				} else if (msg.is(MeasureList.class)) {
+					MeasureList list = msg.unpack(MeasureList.class);
+					for (Measure m : list.getMeasuresList()) {
+						manager.update(new Measurement(m));
+					}
+
+				} else if (msg.is(Welcome.class)) {
+					
+					// nothing to do
+					
+				} else if (msg.is(AskBoost.class)) {
+					AskBoost ask = msg.unpack(AskBoost.class);
+					int boost = manager.getBoostStatus();
+					long t1 = manager.getBoostTimestamp();
+					long t2 = ask.getTimestamp();
+					boolean act = boost == HouseManager.BOOST_ACTIVE;
+					boolean req = boost == HouseManager.BOOST_REQUESTED;
+					boolean busy = act || (req && ( t1 < t2 || ( t1 == t2 && manager.getLocalId().compareTo(id) < 0)));
+					
+					if (busy) {
+						write(Any.pack(DenyBoost.newBuilder().build()));
+					} else {
+						write(Any.pack(GrantBoost.newBuilder().build()));
+					}
+
+				} else if (msg.is(GrantBoost.class)) {
+					
+					//the remote house is not using boost
+					manager.setBoost(id, false);
+					
+				} else if (msg.is(DenyBoost.class)) {
+
+					//the remote house is using boost
+					manager.setBoost(id, true);
+					
+				} else if (msg.is(EndBoost.class)) {
+					
+					manager.setBoost(id, false);
+					
+				} else {
+					System.out.println("Unknown message format");
+				}
+				
+				synchronized(waitLocks) {
+					for (MessageWait w: waitLocks) {
+						w.check(msg);
+					}
+				}
+
+			} catch (InvalidProtocolBufferException e) {
+				System.out.println("Invalid message format");
+				e.printStackTrace();
+			}
+			
+		}
+
+		kill();
+
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ src/main/java/house/HouseManager.java
@@ -0,0 +1,337 @@
+package house;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.protobuf.Any;
+
+import beans.Measurement;
+import proto.HouseProto.AskBoost;
+import proto.HouseProto.EndBoost;
+import proto.HouseProto.Goodbye;
+import proto.HouseProto.Measure;
+import proto.HouseProto.MeasureList;
+import simulator.SmartMeterSimulator;
+import utils.Http;
+
+public class HouseManager {
+
+	private Map<String, HouseConnection> connections = new HashMap<>();
+	private Map<String, Measurement> measures = new HashMap<>();
+
+	private int houseCount = 1;
+	private int measureCount = 0;
+
+	private String localId;
+	private String master;
+
+	private boolean ready = false;
+
+	private ServerSocket socket;
+	private int port;
+
+	public static final int BOOST_INACTIVE = 0;
+	public static final int BOOST_REQUESTED = 2;
+	public static final int BOOST_ACTIVE = 3;
+
+	private int boost = 0;
+	private long boostTimestamp = 0;
+
+	private Map<String, Boolean> boostStatus;
+	private Set<String> pendingBoost;
+
+	private SmartMeterSimulator sim;
+	private Http http;
+
+	public HouseManager(String localId, Http http) throws IOException {
+		this(localId, 0, http);
+	}
+
+	public HouseManager(String localId, int port, Http http) throws IOException {
+		this.localId = localId;
+		this.master = localId;
+		this.http = http;
+
+		measures.put(localId, new Measurement(localId, Double.NaN, 0));
+
+		startWait(port);
+	}
+
+	private void startWait(int port) throws IOException {
+		socket = new ServerSocket(port);
+		this.port = socket.getLocalPort();
+		System.out.println("Listening on " + this.port);
+
+		new Thread(() -> {
+			while(true) {
+				try {
+					Socket conn = socket.accept();
+					new HouseConnection(this, conn);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+	public void setSimulator(SmartMeterSimulator sim) {
+		this.sim = sim;
+	}
+
+	public void addMeasurement(Measurement m) {
+		update(m);
+
+		Any msg = Any.pack(MeasureList.newBuilder().addMeasures(Measure.newBuilder()
+			.setId(m.getId())
+			.setValue(m.getValue())
+			.setTimestamp(m.getTimestamp()))
+			.build());
+
+		for (HouseConnection house: connections.values()) {
+			house.write(msg);
+		}
+		
+		System.out.println(m);
+
+		new Thread(() -> {
+			http.post("stats/add", Arrays.asList(m));
+		});
+
+	}
+
+	public synchronized void add(HouseConnection house) {
+		connections.put(house.getId(), house);
+
+		if (!measures.containsKey(house.getId())) {
+			measures.put(house.getId(), new Measurement(house.getId(), Double.NaN, 0));
+		}
+
+		if (house.getId().compareTo(localId) < 0) {
+			master = house.getId();
+		}
+		houseCount++;
+	}
+
+	public synchronized void remove(String id) {
+		assert (measures.containsKey(id));
+
+		setBoost(id, false);
+
+		Measurement m = measures.get(id);
+		if (!Double.isNaN(m.getValue())) {
+			measureCount--;
+		}
+
+		connections.remove(id);
+		measures.remove(id);
+		if (id.equals(master)) {
+			master = localId;
+			for (String house : connections.keySet()) {
+				if (house.compareTo(master) < 0) {
+					master = house;
+				}
+			}
+		}
+		houseCount--;
+
+		sendSum();
+	}
+
+	public synchronized void update(Measurement m) {
+
+		Measurement old = new Measurement(m.getId(), Double.NaN, 0);
+		int delta = 1;
+
+		if (measures.containsKey(m.getId())) {
+			old = measures.get(m.getId());
+			delta = Double.isNaN(old.getValue()) ? 1 : -1;
+		}
+
+		if (old.compareTo(m) < 0) {
+			measures.put(m.getId(), m);
+			delta += Double.isNaN(m.getValue()) ? -1 : 1;
+			measureCount += delta / 2;
+
+			sendSum();
+		}
+	}
+
+	public synchronized List<Measurement> getMeasurements() {
+		return new ArrayList<Measurement>(measures.values());
+	}
+
+	public synchronized void sendSum() {
+
+		if (ready && isMaster() && houseCount == measureCount) {
+
+			double value = 0;
+			long timestamp = 0;
+			int count = 0;
+
+			List<Measurement> ms = getMeasurements();
+
+			MeasureList.Builder listBuilder = MeasureList.newBuilder();
+			for (Measurement m: ms) {
+				assert (!Double.isNaN(m.getValue()));
+
+				timestamp = Math.max(timestamp, m.getTimestamp());
+				value += m.getValue();
+				count++;
+
+				Measurement m2 = new Measurement(m.getId(), Double.NaN, m.getTimestamp());
+				measures.put(m2.getId(), m2);
+
+				listBuilder.addMeasures(m2.toProtobuf());
+			}
+			measureCount = 0;
+
+			value /= count;
+
+			List<Measurement> total = Arrays.asList(new Measurement("", value, timestamp));
+
+			Any msg = Any.pack(listBuilder.build());
+			new Thread(() -> {
+
+				http.post("stats/add", total);
+
+				for (HouseConnection conn: connections.values()) {
+					conn.write(msg);
+				}
+
+			});
+
+		}
+	}
+
+	public synchronized void stop() {
+
+		sim.stopMeGently();
+
+		http.delete("houses/remove/"+localId);
+
+		for (HouseConnection conn: connections.values()) {
+			Any msg = Any.pack(Goodbye.newBuilder().build());
+			conn.write(msg);
+			conn.kill();
+		}
+
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public synchronized boolean isMaster() {
+		return master.equals(localId);
+	}
+
+	public synchronized void setReady(boolean value) {
+		this.ready = value;
+	}
+
+	public synchronized boolean isReady() {
+		return ready;
+	}
+
+	public String getLocalId() {
+		return localId;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public synchronized long getBoostTimestamp() {
+		return boostTimestamp;
+	}
+
+	public synchronized boolean isBoostActive() {
+		return boost == BOOST_ACTIVE;
+	}
+
+	public synchronized int getBoostStatus() {
+		return boost;
+	}
+
+	public synchronized void boost() {
+		if (boost > BOOST_INACTIVE) return;
+
+		System.out.println("I AM "+localId+" I WANT BOOST");
+
+		boost = BOOST_REQUESTED;
+		boostTimestamp = System.currentTimeMillis();
+
+		pendingBoost = new HashSet<>(connections.keySet());
+		boostStatus = new HashMap<>();
+
+		for (HouseConnection conn: connections.values()) {
+			boostStatus.put(conn.getId(), false);
+			conn.write(
+				Any.pack(AskBoost.newBuilder()
+					.setTimestamp(boostTimestamp)
+					.build()
+				)
+			);
+		}
+
+	}
+
+	public synchronized void canBoost() {
+		assert(boost == BOOST_REQUESTED);
+
+		if (!pendingBoost.isEmpty()) return;
+
+		int count = 0;
+		for (Boolean active: boostStatus.values()) {
+			if (active) count++;
+		}
+
+		if (count < 2) {
+			boost = BOOST_ACTIVE;
+
+			new Thread(() -> {
+				try {
+					System.out.println("I AM "+localId+" BOOOOOST");
+					sim.boost();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				System.out.println("I AM "+localId+" END BOOST :(");
+				stopBoost();
+			});
+
+		}  else {
+			System.out.println("I AM "+localId+" too much boost "+count);
+		}
+	}
+
+	public synchronized void stopBoost() {
+		assert(boost == BOOST_ACTIVE);
+
+		boost = BOOST_INACTIVE;
+
+		for (HouseConnection conn: connections.values()) {
+			conn.write(Any.pack(EndBoost.newBuilder().build()));
+		}
+	}
+
+	public synchronized void setBoost(String id, boolean val) {
+		if (boost != BOOST_REQUESTED) return;
+
+		pendingBoost.remove(id);
+		boostStatus.put(id, val);
+
+		canBoost();
+	}
+}
